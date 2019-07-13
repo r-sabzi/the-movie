8a135a7bfc95f5d26c19c9ce9280c29e26c3e486
r.sabzi <r.kh.sabzi@gmail.com>
2019-07-13 14:07:45 +0430

    add some data fetching to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
 import "./Home.css";
+import { API_KEY, API_URL } from "../../config";
+
 import HeroImage from "../elements/HeroImage/HeroImage";
 import SearchBar from "../elements/SearchBar/SearchBar";
 import FourColGrid from "../elements/FourColGrid/FourColGrid";
-import MovieThumb from "../elements/MovieThumb/MovieThumb";
+// import MovieThumb from "../elements/MovieThumb/MovieThumb";
 import LoadMoreBtn from "../elements/LoadMoreBtn/LoadMoreBtn";
 import Spinner from "../elements/Spinner/Spinner";
 
 export default class Home extends Component {
-  state = {};
+  state = {
+    movies: [],
+    heroImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ""
+  };
+  componentDidMount() {
+    this.setState({ loading: true });
+    const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}`;
+    this.fetchItem(endPoint);
+  }
+  fetchItem = endpoint => {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        console.log(result);
+        this.setState({
+          movies: [...this.state.movies, ...result.results]
+        });
+        console.log(this.state.movies);
+      });
+  };
 
   render() {
     return (
--- src/components/elements/SearchBar/SearchBar.js
+++ src/components/elements/SearchBar/SearchBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import FontAwesome from "react-fontawesome";
+// import FontAwesome from "react-fontawesome";
 import "./SearchBar.css";
 
 export default class SearchBar extends Component {

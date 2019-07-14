a37bbebbbbeccd4f14689b7c3a10bd68efb36965
r.sabzi <r.kh.sabzi@gmail.com>
2019-07-14 13:15:34 +0430

    complete movie page

--- src/components/elements/Actor/Actor.js
+++ src/components/elements/Actor/Actor.js
@@ -2,5 +2,20 @@ import React from "react";
 import { IMAGE_BASE_URL } from "../../../config";
 import "./Actor.css";
 export default function Actor(props) {
-  return <div>Actor</div>;
+  const POSTER_SIZE = "w154";
+
+  return (
+    <div className="rmdb-actor">
+      <img
+        src={
+          props.actor.profile_path
+            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.actor.profile_path}`
+            : "./images/no_image.jpg"
+        }
+        alt="actor thumb"
+      />
+      <span className="rmdb-actor-name">{props.actor.name}</span>
+      <span className="rmdb-actor-character">{props.actor.character}</span>
+    </div>
+  );
 }
--- src/components/elements/Navigation/Navigation.js
+++ src/components/elements/Navigation/Navigation.js
@@ -3,5 +3,15 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 export default function Navigation(props) {
-  return <div>Navigation</div>;
+  return (
+    <div className="rmdb-navigation">
+      <div className="rmdb-navigation-content">
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <p>/</p>
+        <p>{props.movie}</p>
+      </div>
+    </div>
+  );
 }
